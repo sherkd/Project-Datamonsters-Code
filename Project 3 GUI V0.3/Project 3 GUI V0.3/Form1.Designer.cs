e820424b0ff72688ffd45ea91e269eec0a522446
sherkd <skriller96@hotmail.com>
2016-04-07 16:10:36 +0200

    Versie 0.3 Map

---
+++ Project 3 GUI V0.3/Project 3 GUI V0.3/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Project_3_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Stadsdriehoek = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.form2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Oude_Westen = new System.Windows.Forms.Label();
+            this.Cool = new System.Windows.Forms.Label();
+            this.Cs_kwartier = new System.Windows.Forms.Label();
+            this.Rubroek = new System.Windows.Forms.Label();
+            this.Oud_Crooswijk = new System.Windows.Forms.Label();
+            this.Agniesbuurt = new System.Windows.Forms.Label();
+            this.Provenierswijk = new System.Windows.Forms.Label();
+            this.Kop_van_Zuid = new System.Windows.Forms.Label();
+            this.Dijkzigt = new System.Windows.Forms.Label();
+            this.Spangen = new System.Windows.Forms.Label();
+            this.Nieuwe_Werk = new System.Windows.Forms.Label();
+            this.Tussendijken = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.Nieuwe_Western = new System.Windows.Forms.Label();
+            this.Middelland = new System.Windows.Forms.Label();
+            this.NIeuwe_Mathenesse = new System.Windows.Forms.Label();
+            this.Oud_Mathenesse = new System.Windows.Forms.Label();
+            this.Witte_dorp = new System.Windows.Forms.Label();
+            this.Delfshaven = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Stadsdriehoek
+            // 
+            this.Stadsdriehoek.BackColor = System.Drawing.Color.Transparent;
+            this.Stadsdriehoek.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Stadsdriehoek.Location = new System.Drawing.Point(618, 462);
+            this.Stadsdriehoek.Name = "Stadsdriehoek";
+            this.Stadsdriehoek.Size = new System.Drawing.Size(73, 74);
+            this.Stadsdriehoek.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.Stadsdriehoek, "Stadsdriehoek");
+            this.Stadsdriehoek.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1316, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.form2ToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // form2ToolStripMenuItem
+            // 
+            this.form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            this.form2ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.form2ToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.form2ToolStripMenuItem.Text = "Form 2";
+            this.form2ToolStripMenuItem.Click += new System.EventHandler(this.form2ToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Oude_Westen
+            // 
+            this.Oude_Westen.BackColor = System.Drawing.Color.Transparent;
+            this.Oude_Westen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Oude_Westen.Location = new System.Drawing.Point(535, 490);
+            this.Oude_Westen.Name = "Oude_Westen";
+            this.Oude_Westen.Size = new System.Drawing.Size(51, 66);
+            this.Oude_Westen.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.Oude_Westen, "Oude Westen");
+            // 
+            // Cool
+            // 
+            this.Cool.BackColor = System.Drawing.Color.Transparent;
+            this.Cool.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Cool.Location = new System.Drawing.Point(583, 482);
+            this.Cool.Name = "Cool";
+            this.Cool.Size = new System.Drawing.Size(39, 85);
+            this.Cool.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.Cool, "Cool");
+            // 
+            // Cs_kwartier
+            // 
+            this.Cs_kwartier.BackColor = System.Drawing.Color.Transparent;
+            this.Cs_kwartier.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Cs_kwartier.Location = new System.Drawing.Point(511, 462);
+            this.Cs_kwartier.Name = "Cs_kwartier";
+            this.Cs_kwartier.Size = new System.Drawing.Size(75, 28);
+            this.Cs_kwartier.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.Cs_kwartier, "Centraal station kwartier");
+            // 
+            // Rubroek
+            // 
+            this.Rubroek.BackColor = System.Drawing.Color.Transparent;
+            this.Rubroek.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Rubroek.Location = new System.Drawing.Point(646, 431);
+            this.Rubroek.Name = "Rubroek";
+            this.Rubroek.Size = new System.Drawing.Size(37, 31);
+            this.Rubroek.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.Rubroek, "Rubroek");
+            // 
+            // Oud_Crooswijk
+            // 
+            this.Oud_Crooswijk.BackColor = System.Drawing.Color.Transparent;
+            this.Oud_Crooswijk.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Oud_Crooswijk.Location = new System.Drawing.Point(680, 413);
+            this.Oud_Crooswijk.Name = "Oud_Crooswijk";
+            this.Oud_Crooswijk.Size = new System.Drawing.Size(32, 49);
+            this.Oud_Crooswijk.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.Oud_Crooswijk, "Oud Crooswijk");
+            // 
+            // Agniesbuurt
+            // 
+            this.Agniesbuurt.BackColor = System.Drawing.Color.Transparent;
+            this.Agniesbuurt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Agniesbuurt.Location = new System.Drawing.Point(583, 413);
+            this.Agniesbuurt.Name = "Agniesbuurt";
+            this.Agniesbuurt.Size = new System.Drawing.Size(39, 69);
+            this.Agniesbuurt.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.Agniesbuurt, "Agniesbuurt");
+            // 
+            // Provenierswijk
+            // 
+            this.Provenierswijk.BackColor = System.Drawing.Color.Transparent;
+            this.Provenierswijk.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Provenierswijk.Location = new System.Drawing.Point(551, 426);
+            this.Provenierswijk.Name = "Provenierswijk";
+            this.Provenierswijk.Size = new System.Drawing.Size(35, 36);
+            this.Provenierswijk.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.Provenierswijk, "Provenierswijk");
+            // 
+            // Kop_van_Zuid
+            // 
+            this.Kop_van_Zuid.BackColor = System.Drawing.Color.Transparent;
+            this.Kop_van_Zuid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Kop_van_Zuid.Location = new System.Drawing.Point(618, 586);
+            this.Kop_van_Zuid.Name = "Kop_van_Zuid";
+            this.Kop_van_Zuid.Size = new System.Drawing.Size(65, 51);
+            this.Kop_van_Zuid.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.Kop_van_Zuid, "Kop van Zuid");
+            // 
+            // Dijkzigt
+            // 
+            this.Dijkzigt.BackColor = System.Drawing.Color.Transparent;
+            this.Dijkzigt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Dijkzigt.Location = new System.Drawing.Point(553, 567);
+            this.Dijkzigt.Name = "Dijkzigt";
+            this.Dijkzigt.Size = new System.Drawing.Size(69, 19);
+            this.Dijkzigt.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.Dijkzigt, "Dijkzigt");
+            // 
+            // Spangen
+            // 
+            this.Spangen.BackColor = System.Drawing.Color.Transparent;
+            this.Spangen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Spangen.Location = new System.Drawing.Point(384, 490);
+            this.Spangen.Name = "Spangen";
+            this.Spangen.Size = new System.Drawing.Size(73, 66);
+            this.Spangen.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.Spangen, "Spangen");
+            // 
+            // Nieuwe_Werk
+            // 
+            this.Nieuwe_Werk.BackColor = System.Drawing.Color.Transparent;
+            this.Nieuwe_Werk.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Nieuwe_Werk.Location = new System.Drawing.Point(551, 586);
+            this.Nieuwe_Werk.Name = "Nieuwe_Werk";
+            this.Nieuwe_Werk.Size = new System.Drawing.Size(71, 43);
+            this.Nieuwe_Werk.TabIndex = 13;
+            this.toolTip1.SetToolTip(this.Nieuwe_Werk, "Nieuwe Werk");
+            // 
+            // Tussendijken
+            // 
+            this.Tussendijken.BackColor = System.Drawing.Color.Transparent;
+            this.Tussendijken.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Tussendijken.Location = new System.Drawing.Point(393, 586);
+            this.Tussendijken.Name = "Tussendijken";
+            this.Tussendijken.Size = new System.Drawing.Size(77, 29);
+            this.Tussendijken.TabIndex = 14;
+            this.toolTip1.SetToolTip(this.Tussendijken, "Tussendijken");
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label15.Location = new System.Drawing.Point(393, 556);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(77, 30);
+            this.label15.TabIndex = 15;
+            this.label15.Tag = "Bospolder";
+            this.toolTip1.SetToolTip(this.label15, "Bospolder");
+            // 
+            // Nieuwe_Western
+            // 
+            this.Nieuwe_Western.BackColor = System.Drawing.Color.Transparent;
+            this.Nieuwe_Western.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Nieuwe_Western.Location = new System.Drawing.Point(448, 490);
+            this.Nieuwe_Western.Name = "Nieuwe_Western";
+            this.Nieuwe_Western.Size = new System.Drawing.Size(64, 96);
+            this.Nieuwe_Western.TabIndex = 16;
+            this.toolTip1.SetToolTip(this.Nieuwe_Western, "Nieuwe Western");
+            // 
+            // Middelland
+            // 
+            this.Middelland.BackColor = System.Drawing.Color.Transparent;
+            this.Middelland.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Middelland.Location = new System.Drawing.Point(497, 490);
+            this.Middelland.Name = "Middelland";
+            this.Middelland.Size = new System.Drawing.Size(38, 98);
+            this.Middelland.TabIndex = 17;
+            this.toolTip1.SetToolTip(this.Middelland, "Middelland");
+            // 
+            // NIeuwe_Mathenesse
+            // 
+            this.NIeuwe_Mathenesse.BackColor = System.Drawing.Color.Transparent;
+            this.NIeuwe_Mathenesse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.NIeuwe_Mathenesse.Location = new System.Drawing.Point(309, 556);
+            this.NIeuwe_Mathenesse.Name = "NIeuwe_Mathenesse";
+            this.NIeuwe_Mathenesse.Size = new System.Drawing.Size(90, 81);
+            this.NIeuwe_Mathenesse.TabIndex = 18;
+            this.toolTip1.SetToolTip(this.NIeuwe_Mathenesse, "Nieuwe mathenesse");
+            // 
+            // Oud_Mathenesse
+            // 
+            this.Oud_Mathenesse.BackColor = System.Drawing.Color.Transparent;
+            this.Oud_Mathenesse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Oud_Mathenesse.Location = new System.Drawing.Point(309, 490);
+            this.Oud_Mathenesse.Name = "Oud_Mathenesse";
+            this.Oud_Mathenesse.Size = new System.Drawing.Size(90, 77);
+            this.Oud_Mathenesse.TabIndex = 19;
+            this.toolTip1.SetToolTip(this.Oud_Mathenesse, "Oud mathenesse");
+            // 
+            // Witte_dorp
+            // 
+            this.Witte_dorp.BackColor = System.Drawing.Color.Transparent;
+            this.Witte_dorp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Witte_dorp.Location = new System.Drawing.Point(468, 586);
+            this.Witte_dorp.Name = "Witte_dorp";
+            this.Witte_dorp.Size = new System.Drawing.Size(67, 55);
+            this.Witte_dorp.TabIndex = 20;
+            this.toolTip1.SetToolTip(this.Witte_dorp, "Witte dorp");
+            // 
+            // Delfshaven
+            // 
+            this.Delfshaven.BackColor = System.Drawing.Color.Transparent;
+            this.Delfshaven.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Delfshaven.Location = new System.Drawing.Point(393, 615);
+            this.Delfshaven.Name = "Delfshaven";
+            this.Delfshaven.Size = new System.Drawing.Size(77, 30);
+            this.Delfshaven.TabIndex = 21;
+            this.toolTip1.SetToolTip(this.Delfshaven, "Delfshaven");
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1316, 855);
+            this.Controls.Add(this.Delfshaven);
+            this.Controls.Add(this.Witte_dorp);
+            this.Controls.Add(this.Oud_Mathenesse);
+            this.Controls.Add(this.NIeuwe_Mathenesse);
+            this.Controls.Add(this.Middelland);
+            this.Controls.Add(this.Nieuwe_Western);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.Tussendijken);
+            this.Controls.Add(this.Nieuwe_Werk);
+            this.Controls.Add(this.Spangen);
+            this.Controls.Add(this.Dijkzigt);
+            this.Controls.Add(this.Kop_van_Zuid);
+            this.Controls.Add(this.Provenierswijk);
+            this.Controls.Add(this.Agniesbuurt);
+            this.Controls.Add(this.Oud_Crooswijk);
+            this.Controls.Add(this.Rubroek);
+            this.Controls.Add(this.Cs_kwartier);
+            this.Controls.Add(this.Cool);
+            this.Controls.Add(this.Oude_Westen);
+            this.Controls.Add(this.Stadsdriehoek);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Stadsdriehoek;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem form2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label Oude_Westen;
+        private System.Windows.Forms.Label Cool;
+        private System.Windows.Forms.Label Cs_kwartier;
+        private System.Windows.Forms.Label Rubroek;
+        private System.Windows.Forms.Label Oud_Crooswijk;
+        private System.Windows.Forms.Label Agniesbuurt;
+        private System.Windows.Forms.Label Provenierswijk;
+        private System.Windows.Forms.Label Kop_van_Zuid;
+        private System.Windows.Forms.Label Dijkzigt;
+        private System.Windows.Forms.Label Spangen;
+        private System.Windows.Forms.Label Nieuwe_Werk;
+        private System.Windows.Forms.Label Tussendijken;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label Nieuwe_Western;
+        private System.Windows.Forms.Label Middelland;
+        private System.Windows.Forms.Label NIeuwe_Mathenesse;
+        private System.Windows.Forms.Label Oud_Mathenesse;
+        private System.Windows.Forms.Label Witte_dorp;
+        private System.Windows.Forms.Label Delfshaven;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Project 3 GUI V0.3/Project 3 GUI V0.3/Form2.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Project_3_GUI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.form1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.form1ToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.fileToolStripMenuItem.Text = "Menu";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // form1ToolStripMenuItem
+            // 
+            this.form1ToolStripMenuItem.Name = "form1ToolStripMenuItem";
+            this.form1ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.form1ToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.form1ToolStripMenuItem.Text = "Form1";
+            this.form1ToolStripMenuItem.Click += new System.EventHandler(this.form1ToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(882, 855);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem form1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
