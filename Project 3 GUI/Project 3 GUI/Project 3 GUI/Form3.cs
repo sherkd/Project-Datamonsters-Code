766ac34dee796c362caffc657aa87674b4e15204
gkygrek <t.c.o.verhagen@gmail.com>
2016-04-07 16:09:01 +0200

    nieuwe code

--- Project 3 GUI/Project 3 GUI/Project 3 GUI/Form3.cs
+++ Project 3 GUI/Project 3 GUI/Project 3 GUI/Form3.cs
@@ -28,5 +28,24 @@ namespace Project_3_GUI
         {
             Application.Exit();
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form4 newForm = new Form4();
+            newForm.Show();
+            this.Hide();
+        }
+
+        private void straatroven_Click(object sender, EventArgs e)
+        {
+            Form5 newForm = new Form5();
+            newForm.Show();
+            this.Hide();
+        }
     }
 }
